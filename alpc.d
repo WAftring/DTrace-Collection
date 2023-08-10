7f53177e016ed483027660aaeee2296c373d0181
Will Aftring <william.aftring@outlook.com>
2023-08-10 11:21:37 -0400

    Update alpc.d

--- alpc.d
+++ alpc.d
@@ -1,32 +1,17 @@
+#pragma D option quiet
 
-/* NtAlpcAcceptConnectPort Params
+/* NtAlpcConnectPort Params
  * arg0 OUT PHANDLE PortHandle
  * arg1 IN PUNICODE_STRING PortName
  * arg2 IN POBJECT_ATTRIBUTES ObjectAttributes
  * arg3 OPTIONAL PALPC_PORT_ATTRIBUTES PortAttributes
  * arg4 IN ULONG Flags
- * TODO(will): Fix this
  */
 
-::NtAlpcConnectPort:entry
+syscall::NtAlpcConnectPort:entry
 {
-    this->us = (PUNICODE_STRING)arg1;
-    this->val = wstr2str(copyin(this->us->Buffer, this->us->Length) / 2);
-    printf("%s (%d) NtAlpcConnectPort %s called\n", execname, pid, this->val);
-}
-
-/* NtAlpcAcceptConnectPort Params
- * arg0 _Out_ PHANDLE  	PortHandle,
- * arg1 _In_ HANDLE  	ConnectionPortHandle,
- * arg2 _In_ ULONG  	Flags,
- * arg3 _In_opt_ POBJECT_ATTRIBUTES  	ObjectAttributes,
- * arg4 _In_opt_ PALPC_PORT_ATTRIBUTES  	PortAttributes,
- * arg5 _In_opt_ PVOID  	PortContext,
- * arg6 _In_reads_bytes_(ConnectionRequest->u1.s1.TotalLength) PPORT_MESSAGE  	ConnectionRequest,
- * arg7 _Inout_opt_ PALPC_MESSAGE_ATTRIBUTES  	ConnectionMessageAttributes,
- * arg8 _In_ BOOLEAN 
- */
-::NtAlpcAcceptConnectPort:entry
-{
-  printf("%s (%d) NtAlpcAcceptConnectPort\n", execname, pid);
+  this->PortName = (nt`UNICODE_STRING*)copyin(arg1, sizeof(nt`UNICODE_STRING));
+  this->wBuffer = (uint16_t*) copyin((uintptr_t)this->PortName->Buffer, this->PortName->Length);
+  this->aBuffer = wstr2str((wchar_t*)this->wBuffer, this->PortName->Length / 2);
+  printf("%s (%d) NtAlpcConnectPort %s called\n", execname, pid, this->aBuffer);
 }
