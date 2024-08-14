5d97bf44c6cc52764588e8f38dd7f8f4f845c24a
Will Aftring <wiaftrin@microsoft.com>
2024-08-14 11:24:58 -0400

    Adding users and connection options

--- PowerShell/Setup-VM.ps1
+++ PowerShell/Setup-VM.ps1
@@ -1,4 +1,11 @@
 #Requires -RunAsAdministrator
+$Script:UserName = "Will"
+function Create-MyUser {
+    $password = Read-Host -AsSecureString "Enter Password"
+    New-LocalUser -Name $Script:UserName -Password $password -FullName $Script:UserName -Description "Standard User"
+    Add-LocalGroupMember -Group "Administrators" -Member $Script:UserName
+}
+
 function Create-CoreFolders {
     $Folders = "C:\Tools", "C:\Scratch", "C:\Data", "C:\Public", "C:\Dumps"
     $Folders | ForEach-Object {
@@ -27,6 +34,41 @@ function Setup-Shares {
     New-SmbShare -Name "Public" -Path "C:\Public" -FullAccess "Everyone"
 }
 
+function Setup-SSH {
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    # Start the sshd service
+    Start-Service sshd
+
+    # OPTIONAL but recommended:
+    Set-Service -Name sshd -StartupType 'Automatic'
+
+    # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+    if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+        Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+        New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+    } else {
+        Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+    }
+
+    # Perform the replacement
+    $ConfigPath = "C:\ProgramData\ssh\sshd_config"
+    $fileContent = Get-Content C:\ProgramData\ssh\sshd_config
+    $fileContent = $fileContent -replace '^Subsystem\s+sftp\s+sftp-server\.exe$', 'Subsystem   sftp    sftp-server.exe -d "c:\Public"'
+    Set-Content $ConfigPath $fileContent
+
+
+# Write the updated content back to the file
+Set-Content -Path $filePath -Value $fileContent
+
+}
+
+function Setup-RDP {
+    # Enable Remote Desktop
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+    Add-LocalGroupMember -Group "Remote Desktop Users" -Member $Script:UserName
+}
+
 function Setup-RegistryKeys {    
     reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 1 /f
     reg add HKLM\Software\Policies\Microsoft\WindowsStore /v AutoDownload /t REG_DWORD /d 2 /f
@@ -40,6 +82,8 @@ function Setup-RegistryKeys {
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" /v DumpType /t REG_DWORD /d 2 /f
 }
 
+Write-Host "Creating core user"
+Create-MyUser
 Write-Host "Setting up basic folder structure"
 Create-CoreFolders
 Write-Host "Downloading required software"
@@ -51,6 +95,10 @@ Write-Host "Setting up registry keys"
 Setup-RegistryKeys
 Write-Host "Setting up SMB Shares"
 Setup-Shares
+Write-Host "Setting up OpeNSSH"
+Setup-SSH
+Write-Host "Setup RDP"
+Setup-RDP
 Write-Host "Creating PS Profile"
 New-Item $PROFILE -Force
 @"
