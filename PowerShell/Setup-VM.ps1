38ad6df704b331144d8787f974d3aa710c1caae3
Will Aftring <wiaftrin@microsoft.com>
2024-09-26 15:02:07 -0400

    Add DC work

--- PowerShell/Setup-VM.ps1
+++ PowerShell/Setup-VM.ps1
@@ -1,4 +1,8 @@
 #Requires -RunAsAdministrator
+param(
+    [switch]$DC
+)
+
 $Script:UserName = "Will"
 function Create-MyUser {
     $password = Read-Host -AsSecureString "Enter Password"
@@ -34,34 +38,6 @@ function Setup-Shares {
     New-SmbShare -Name "Public" -Path "C:\Public" -FullAccess "Everyone"
 }
 
-function Setup-SSH {
-    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-    # Start the sshd service
-    Start-Service sshd
-
-    # OPTIONAL but recommended:
-    Set-Service -Name sshd -StartupType 'Automatic'
-
-    # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-    if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-        Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-        New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-    } else {
-        Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-    }
-
-    # Perform the replacement
-    $ConfigPath = "C:\ProgramData\ssh\sshd_config"
-    $fileContent = Get-Content C:\ProgramData\ssh\sshd_config
-    $fileContent = $fileContent -replace '^Subsystem\s+sftp\s+sftp-server\.exe$', 'Subsystem   sftp    sftp-server.exe -d "c:\Public"'
-    Set-Content $ConfigPath $fileContent
-
-
-# Write the updated content back to the file
-Set-Content -Path $filePath -Value $fileContent
-
-}
-
 function Setup-RDP {
     # Enable Remote Desktop
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
@@ -82,6 +58,13 @@ function Setup-RegistryKeys {
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" /v DumpType /t REG_DWORD /d 2 /f
 }
 
+function Setup-DC {
+    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+    $domain = Read-Host -Prompt "Domain name?"
+    $pw = Read-Host -Prompt "SafeMode Password" -AsSecureString
+    Install-ADDSForest -DomainName $domain -SafeModeAdministratorPassword $pw -InstallDNS -Force -Confirm:$false
+}
+
 Write-Host "Creating core user"
 Create-MyUser
 Write-Host "Setting up basic folder structure"
@@ -95,8 +78,6 @@ Write-Host "Setting up registry keys"
 Setup-RegistryKeys
 Write-Host "Setting up SMB Shares"
 Setup-Shares
-Write-Host "Setting up OpeNSSH"
-Setup-SSH
 Write-Host "Setup RDP"
 Setup-RDP
 Write-Host "Creating PS Profile"
@@ -108,7 +89,12 @@ function e {
 }
 "@ | Out-File $PROFILE
 
+if ($DC) {
+    Write-Host "Setup Domain Controller"
+    Setup-DC
+}
 
+Rename-Computer
 Read-Host "Press Enter to restart"
 
 Restart-Computer -Force
